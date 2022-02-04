4e1b3619ed1eb74c802868cb1e59977f6a7b5ee9
lazarofonseca <lazaronobrega3@gmail.com>
2022-02-04 19:15:34 -0300

    Bonificação Funcionário v0.1

--- ProjetoBonificacaoFuncionario/src/br/com/lazaro/aplicacao/Aplicacao.java
+++ ProjetoBonificacaoFuncionario/src/br/com/lazaro/aplicacao/Aplicacao.java
@@ -1,9 +1,52 @@
 package br.com.lazaro.aplicacao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import br.com.lazaro.entidade.Funcionario;
+
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		
+
+		Locale.setDefault(Locale.getDefault());
+		List<Funcionario> listaFuncionarios = new ArrayList<Funcionario>();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Quantos funcionários deseja cadastrar? ");
+		Integer cont = sc.nextInt();
+		sc.nextLine();
+
+		for (int i = 0; i < cont; i++) {
+			System.out.println("Digite o nome do funcionário: ");
+			String nome = sc.nextLine();
+			System.out.println("Digite o valor do salário: ");
+			Double salario = sc.nextDouble();
+			sc.nextLine();
+			Funcionario funcionario = new Funcionario(nome, salario);
+			funcionario.calculaSalario(funcionario.getSalario());
+			listaFuncionarios.add(funcionario);
+
+		}
+		System.out.println();
+		System.out.println("------------Lista de funcionários e seus dados financeiros-----------");
+		System.out.println();
+		for (Funcionario lista : listaFuncionarios) {
+			System.out.println("Funcionário " + lista.getNome());
+			System.out.printf("Salário %.2f :\n", lista.getSalario());
+			if(lista.getSalario() <= 2000.0) {
+			System.out.printf("Bonus %.2f : \n", lista.getBonus());
+			}else if(lista.getSalario() > 2000.0){
+				System.out.printf("Desconto %.2f : \n", lista.getBonus());
+			}
+			
+			System.out.printf("Salário Liquido %.2f : \n", lista.getSalarioLiquido());
+			System.out.println();
+
+		}
+
+		sc.close();
 
 	}
 
--- ProjetoBonificacaoFuncionario/src/br/com/lazaro/entidade/Funcionario.java
+++ ProjetoBonificacaoFuncionario/src/br/com/lazaro/entidade/Funcionario.java
@@ -1,7 +1,68 @@
 package br.com.lazaro.entidade;
 
 public class Funcionario {
-	
-	
 
+	private String nome;
+	private Double salario;
+	private Double bonus;
+	private Double salarioLiquido;
+
+	public Funcionario() {
+	}
+
+	public Funcionario(String nome, Double salario) {
+		this.nome = nome;
+		this.salario = salario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public Double getBonus() {
+		return bonus;
+	}
+
+	public void setBonus(Double bonus) {
+		this.bonus = bonus;
+	}
+
+	public Double getSalarioLiquido() {
+		return salarioLiquido;
+	}
+
+	public void setSalarioLiquido(Double salarioLiquido) {
+		this.salarioLiquido = salarioLiquido;
+	}
+
+	public Double calculaSalario(Double salario) {
+		this.salario = salario;
+		if (this.salario > 0 && this.salario <= 1000.0) {
+			
+			this.bonus = (this.salario * 0.20);
+
+			return this.salarioLiquido = (this.bonus + salario);
+		} else if (this.salario > 1000.0 && this.salario <= 2000.0) {
+
+			this.bonus = (salario * 0.10);
+
+			return this.salarioLiquido = (this.bonus + salario);
+		} else{
+			this.salario = salario;
+			this.bonus = salario * 0.10;
+			return this.salarioLiquido = (this.salario - (this.salario * 0.10));
+		}
+	}
 }
