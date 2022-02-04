5ab5042e5348b93b5f245f11b574f263ac7e61f3
lazarofonseca <lazaronobrega3@gmail.com>
2022-02-04 12:26:26 -0300

    Login V0.1

--- ProjetoValidaLogin/src/br/com/lazaro/app/Aplicacao.java
+++ ProjetoValidaLogin/src/br/com/lazaro/app/Aplicacao.java
@@ -1,7 +1,8 @@
 package br.com.lazaro.app;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -14,28 +15,66 @@ public class Aplicacao {
 
 		Locale.setDefault(Locale.US);
 
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
 		Scanner sc = new Scanner(System.in);
-		List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+		// List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+		Login loginSenha = new Login();
+
 		Usuario usuario0 = new Usuario("lazaro", "123");
+		loginSenha.adicionaUsuario(usuario0);
 		Usuario usuario1 = new Usuario("teste1", "87540");
+		loginSenha.adicionaUsuario(usuario1);
 		Usuario usuario2 = new Usuario("teste2", "9743");
+		loginSenha.adicionaUsuario(usuario2);
 		Usuario usuario3 = new Usuario("teste3", "fddfu3");
+		loginSenha.adicionaUsuario(usuario3);
 		Usuario usuario4 = new Usuario("teste4", "63483");
+		loginSenha.adicionaUsuario(usuario4);
 
-		listaUsuarios.add(usuario0);
-		listaUsuarios.add(usuario1);
-		listaUsuarios.add(usuario2);
-		listaUsuarios.add(usuario3);
-		listaUsuarios.add(usuario4);
+		System.out.println("Digite seu usuário:");
+		String login = sc.next();
+		System.out.println("Digite sua senha: ");
+		String senha = sc.next();
 
-		sc.close();
+		boolean encontrou = true;
 
-		for (int i = 0; i < listaUsuarios.size(); i++) {
-			System.out.println("Usuário e senha");
-			System.out.println("Usuário : " + listaUsuarios.get(i).getNome() + " Senha ; " + 
-			listaUsuarios.get(i).getSenha());
+		for (int i = 0; i < loginSenha.getUsuarios().size(); i++) {
+
+			if (loginSenha.getUsuarios().get(i).getNome().equals(login)
+					&& loginSenha.getUsuarios().get(i).getSenha().equals(senha)) {
+				// System.out.println("Bem vindo!");
+				encontrou = true;
+				break;
+			} else {
+				encontrou = false;
+			}
+
 		}
 
+		Date horaAtual = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(horaAtual);
+		int hora = cal.get(Calendar.HOUR_OF_DAY);
+		int minuto = cal.get(Calendar.MINUTE);
+
+		System.out.println(encontrou);
+
+		if (encontrou && hora > 6 && hora < 11 && minuto <= 59) {
+			System.out.println("Bom dia, você se logou com sucesso.");
+		} else if (encontrou && hora > 11 && hora < 17 && minuto <= 59) {
+			System.out.println("Boa tarde, você se logou com sucesso.");
+		} else if (encontrou && hora > 17 && hora < 23 && minuto <= 59) {
+			System.out.println("Boa noite, você se logou ao nosso sistema.");
+		} else if (encontrou && hora == 00 && hora < 5 && minuto <= 59) {
+			System.out.println("Boa madrugada, você se logou ao nosso sistema.");
+		} else {
+			System.out.println("Usuário e/ou senha incorretos.");
+		}
+
+	
+		sc.close();
+		
 	}
 
 }
